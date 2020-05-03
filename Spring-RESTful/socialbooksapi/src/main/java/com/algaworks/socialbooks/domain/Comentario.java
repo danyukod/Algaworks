c95cab7c41247b26ebd5437042dde8cde983f32a
DaniloKodavara <danilo.kodavara@hotmail.com>
2020-05-03 17:52:27 -0300

    Criando uma representaçcao para o recurso livro

--- Spring-RESTful/socialbooksapi/src/main/java/com/algaworks/socialbooks/domain/Comentario.java
+++ Spring-RESTful/socialbooksapi/src/main/java/com/algaworks/socialbooks/domain/Comentario.java
@@ -1,4 +1,46 @@
 package com.algaworks.socialbooks.domain;
 
+import java.util.Date;
+
 public class Comentario {
+
+    private Long id;
+
+    private String text;
+
+    private String usuario;
+
+    private Date data;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
 }
