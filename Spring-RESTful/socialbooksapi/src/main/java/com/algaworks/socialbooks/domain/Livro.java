08a0b5f29134f4e2dcd880c7e614d14198496e24
DaniloKodavara <danilo.kodavara@hotmail.com>
2020-05-04 21:54:08 -0300

    Utilizando a anotação @JsonInclude

--- Spring-RESTful/socialbooksapi/src/main/java/com/algaworks/socialbooks/domain/Livro.java
+++ Spring-RESTful/socialbooksapi/src/main/java/com/algaworks/socialbooks/domain/Livro.java
@@ -1,22 +1,31 @@
 package com.algaworks.socialbooks.domain;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 import java.util.Date;
 import java.util.List;
 
 public class Livro {
 
+    @JsonInclude(Include.NON_NULL)
     private Long id;
 
     private String nome;
 
+    @JsonInclude(Include.NON_NULL)
     private Date publicacao;
 
+    @JsonInclude(Include.NON_NULL)
     private String editora;
 
+    @JsonInclude(Include.NON_NULL)
     private String resumo;
 
+    @JsonInclude(Include.NON_NULL)
     private List<Comentario> comentarios;
 
+    @JsonInclude(Include.NON_NULL)
     private String autor;
 
     public Livro() {
