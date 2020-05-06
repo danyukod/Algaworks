411df90744b11042c18cf320b30ac88596b93d55
DaniloKodavara <danilo.kodavara@hotmail.com>
2020-05-05 22:56:17 -0300

    Finalizando o tratamento das respostas

--- Spring-RESTful/socialbooksapi/src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
+++ Spring-RESTful/socialbooksapi/src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
@@ -3,6 +3,7 @@ package com.algaworks.socialbooks.resources;
 import com.algaworks.socialbooks.domain.Livro;
 import com.algaworks.socialbooks.repository.LivrosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,8 +21,8 @@ public class LivrosResources {
     private LivrosRepository livrosRepository;
 
     @RequestMapping(method = RequestMethod.GET)
-    public List<Livro> listar() {
-        return livrosRepository.findAll();
+    public ResponseEntity<List<Livro>> listar() {
+        return ResponseEntity.status(HttpStatus.OK).body(livrosRepository.findAll());
     }
 
     @RequestMapping(method = RequestMethod.POST)
@@ -42,13 +43,19 @@ public class LivrosResources {
 
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    public void deletar(@PathVariable("id") Long id) {
-        livrosRepository.deleteById(id);
+    public ResponseEntity<Void> deletar(@PathVariable("id") Long id) {
+        try {
+            livrosRepository.deleteById(id);
+        }catch (EmptyResultDataAccessException e){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.noContent().build();
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-    public void atualizar(@RequestBody Livro livro, @PathVariable Long id) {
+    public ResponseEntity<Void> atualizar(@RequestBody Livro livro, @PathVariable Long id) {
         livro.setId(id);
         livrosRepository.save(livro);
+        return ResponseEntity.noContent().build();
     }
 }
