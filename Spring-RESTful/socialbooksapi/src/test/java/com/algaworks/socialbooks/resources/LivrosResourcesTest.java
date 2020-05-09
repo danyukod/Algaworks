25d4fbd78bebffc02ac0a3222b7a993abe3bad7e
DaniloKodavara <danilo.kodavara@hotmail.com>
2020-05-09 19:13:11 -0300

    Adicionando mÃetodo para testar pagina nao encontrada ao pedir para listar filmes

--- Spring-RESTful/socialbooksapi/src/test/java/com/algaworks/socialbooks/resources/LivrosResourcesTest.java
+++ Spring-RESTful/socialbooksapi/src/test/java/com/algaworks/socialbooks/resources/LivrosResourcesTest.java
@@ -2,7 +2,6 @@ package com.algaworks.socialbooks.resources;
 
 import com.algaworks.socialbooks.services.LivrosService;
 import io.restassured.http.ContentType;
-import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +9,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 
-import static io.restassured.module.mockmvc.RestAssuredMockMvc.*;
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.standaloneSetup;
 
 @WebMvcTest
 public class LivrosResourcesTest {
@@ -22,18 +22,28 @@ public class LivrosResourcesTest {
     private LivrosService livrosService;
 
     @BeforeEach
-    public void setup(){
+    public void setup() {
         standaloneSetup(this.livrosResources);
     }
 
     @Test
-    public void deveRetornarSucesso_QuandoListarFilmes(){
+    public void deveRetornarSucesso_QuandoListarFilmes() {
         given()
                 .accept(ContentType.JSON)
                 .when()
                 .get("/livros")
                 .then()
                 .statusCode(HttpStatus.OK.value());
+    }
+
+    @Test
+    void deveRetornarPaginaNaoEncontrada_QuandoListarFilmes() {
+
+        given()
+                .accept(ContentType.JSON)
+                .when().get("/filmes")
+                .then()
+                .statusCode(HttpStatus.NOT_FOUND.value());
     }
 
 }
